4c45b175543446b6e7977d9e8188b459bdcd5899
Agam main <work.agamchopra@gmail.com>
2023-10-22 17:03:46 -0700

    Relay class added and tested in relay_action.py for communication with external arduino mouse/keyboard action relay

--- code/relay_actions.py
+++ code/relay_actions.py
@@ -7,30 +7,50 @@ Created on October 2023
 import serial.tools.list_ports
 
 
-ports = serial.tools.list_ports.comports()
-serialInst = serial.Serial()
+class Relay():
+    def __init__(self, channel=9600):
+        self.channel = channel
+        self.ports = serial.tools.list_ports.comports()
+        self.serialInst = serial.Serial()
+        self.portsList = []
+        for onePort in self.ports:
+            self.portsList.append(str(onePort))
 
-portsList = []
+        self.portVar = None
 
-for onePort in ports:
-    portsList.append(str(onePort))
-    print(str(onePort))
+    def lookup_ports(self):
+        for onePort in self.portsList:
+            print(onePort)
 
-val = input("Serial Port: COM")
+    def activate_port(self, port_number):
+        for p in self.portsList:
+            if p.startswith("COM" + str(port_number)):
+                self.portVar = "COM" + str(port_number)
 
-for p in portsList:
-    if p.startswith("COM" + str(val)):
-        portVar = "COM" + str(val)
-        print(portVar)
+        self.serialInst.baudrate = self.channel
+        self.serialInst.port = self.portVar
+        self.serialInst.open()
 
-serialInst.baudrate = 9600
-serialInst.port = portVar
-serialInst.open()
+    def free_port(self):
+        self.serialInst.close()
 
-while (True):
-    command = input("Command: ")
-    serialInst.write(command.encode('utf-8'))
+    def send_action(self, action='0,0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0'):
+        self.serialInst.write(action.encode('utf-8'))
 
-    if command == 'p':
-        serialInst.close()
-        break
+
+def test():
+    relay = Relay()
+    relay.lookup_ports()
+    relay.activate_port(input('Port #: '))
+
+    while (True):
+        command = input("Command: ")
+        relay.send_action(command)
+
+        if command == 'p':
+            relay.free_port()
+            break
+
+
+if __name__ == '__main__':
+    test()
